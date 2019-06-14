91ae52ac730a4965ef5430ca85fd6c5ccc526fe6
Matt Coy <fluffytoycoy@gmail.com>
2019-06-13 19:49:09 -0500

    added actual RBP image for hero and fixed margin to account for new image

--- src/componets/Home/Hero/Hero.js
+++ src/componets/Home/Hero/Hero.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Hero.scss';
+const imgUrl = require ('../../.././img/RBP.png');
 
 function Hero() {
   return (
     <section id="hero">
       <div className="hero-bg">
         <div className="rbp-img">
-          <img src="https://media.discordapp.net/attachments/477927083809439786/587123895597793311/RBPBIGNEO75.png" alt="retro block party"/>
+          <img src={imgUrl} alt="retro block party"/>
         </div>
         <div className="rbp-img2">
           <h2><span>B</span>RINGING <span>R</span>ETRO <span>T</span>OGETHER<br/><span>F</span>OR <span>C</span>HARITY</h2>
